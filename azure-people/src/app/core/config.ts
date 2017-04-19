a203587ff3e119d4053941c929b65d418bd9e1c8
Jeff Etter <jetter@epsad.com>
2017-04-19 11:45:07 -0500

    commit

--- azure-people/src/app/core/config.ts
+++
@@ -1,8 +0,0 @@
-export const CONFIG = {
-  baseUrls: {
-    localHost:"http://localhost:1933/api/",
-    azure:""
-  }
-};
-
-export let ENDPOINT = CONFIG.baseUrls.localHost;
--- azure-people/src/app/shared/models/people.ts
+++
@@ -1,9 +0,0 @@
-export class PeopleListItem{
-    constructor(
-        public id:string,
-        public name:string,
-        public email:string,
-        public phone:string,
-        public personType:string
-    ){}
-}
