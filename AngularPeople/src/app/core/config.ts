11a6bf564a8fa12c2334c94572d2ab6086ab48e3
Jeff Etter <jetter@epsad.com>
2017-04-19 16:22:25 -0500

    Added web.config for Azure support

--- AngularPeople/src/app/core/config.ts
+++ AngularPeople/src/app/core/config.ts
@@ -1,7 +1,7 @@
 export const CONFIG = {
   baseUrls: {
     localHost:"http://localhost:1933/api/",
-    azure:""
+    azure:"http://eps-azureboot2017-person-service.azurewebsites.net/api/"
   }
 };
 
