e9c497417baf393c897b88cd1d961a70b451b640
Jeff Etter <jetter@epsad.com>
2017-04-19 18:48:36 -0500

    commit

---
+++ AzureWebServices/Core.Services/Contracts/PersonInformation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace PersonService.Contracts
+{
+    [DataContract]
+    public class PersonListInformation
+    {
+        [DataMember(IsRequired = true)] public Guid Id { get; set; }
+        [DataMember(IsRequired = true)] public string Name { get; set; }
+        [DataMember(IsRequired = true)] public string Email { get; set; }
+        [DataMember(IsRequired = true)] public string Phone { get; set; }
+        [DataMember(IsRequired = true)] public string PersonType { get; set; }
+    }
+
+    [DataContract]
+    public class PersonDetailInformation
+    {
+        [DataMember(IsRequired = true)] public Guid Id { get; set; }
+        [DataMember(IsRequired = true)] public string FirstName { get; set; }
+        [DataMember(IsRequired = true)] public string LastName { get; set; }
+        [DataMember(IsRequired = true)] public string Email { get; set; }
+        [DataMember(IsRequired = true)] public string Phone { get; set; }
+        [DataMember(IsRequired = true)] public Guid PersonTypeId { get; set; }
+    }
+
+    [DataContract]
+    public class PersonTypeInformation
+    {
+        [DataMember(IsRequired = true)] public Guid Id { get; set; }
+        [DataMember(IsRequired = true)] public string Description { get; set; }
+    }
+}
---
+++ AzureWebServices/Core.Services/EFModels/DockerSampleContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Core.Services.EFModels
+{
+    public partial class DockerSampleContext : DbContext
+    {
+        public virtual DbSet<Person> Person { get; set; }
+        public virtual DbSet<PersonType> PersonType { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+            //optionsBuilder.UseSqlServer(@"Server=localhost;Database=DockerSample;User Id=Doc_DbUser;Password=P@ssw0rd");
+            optionsBuilder.UseSqlServer(@"Server=eps-azurebootcamp.database.windows.net;Database=DockerSample;User Id=Doc_DbUser;Password=P@ssw0rd");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Person>(entity =>
+            {
+                entity.ToTable("Person", "dbo");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(150);
+
+                entity.Property(e => e.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Phone)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.PersonType)
+                    .WithMany(p => p.Person)
+                    .HasForeignKey(d => d.PersonTypeId)
+                    .HasConstraintName("fk_PersonType_Person");
+            });
+
+            modelBuilder.Entity<PersonType>(entity =>
+            {
+                entity.ToTable("PersonType", "dbo");
+
+                entity.Property(e => e.Id)
+                    .HasColumnName("id")
+                    .ValueGeneratedNever();
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(50);
+            });
+        }
+    }
+}
